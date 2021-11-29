78d9275cc0e61098a2c8f60697909045750b74b7
Andy Nenthong <C0516154@intra.camosun.bc.ca>
2021-11-29 11:04:25 -0800

    Add project files.

---
+++ Problem10/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Problem10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoLargeSize = new System.Windows.Forms.RadioButton();
+            this.rdoMediumSize = new System.Windows.Forms.RadioButton();
+            this.rdoSmallSize = new System.Windows.Forms.RadioButton();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoLargeSize);
+            this.groupBox1.Controls.Add(this.rdoMediumSize);
+            this.groupBox1.Controls.Add(this.rdoSmallSize);
+            this.groupBox1.Location = new System.Drawing.Point(92, 76);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(136, 132);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pizza size";
+            // 
+            // rdoLargeSize
+            // 
+            this.rdoLargeSize.AutoSize = true;
+            this.rdoLargeSize.Location = new System.Drawing.Point(29, 92);
+            this.rdoLargeSize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.rdoLargeSize.Name = "rdoLargeSize";
+            this.rdoLargeSize.Size = new System.Drawing.Size(80, 19);
+            this.rdoLargeSize.TabIndex = 2;
+            this.rdoLargeSize.Text = "large ($20)";
+            this.rdoLargeSize.UseVisualStyleBackColor = true;
+            this.rdoLargeSize.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // rdoMediumSize
+            // 
+            this.rdoMediumSize.AutoSize = true;
+            this.rdoMediumSize.Location = new System.Drawing.Point(29, 65);
+            this.rdoMediumSize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.rdoMediumSize.Name = "rdoMediumSize";
+            this.rdoMediumSize.Size = new System.Drawing.Size(99, 19);
+            this.rdoMediumSize.TabIndex = 1;
+            this.rdoMediumSize.Text = "medium ($15)";
+            this.rdoMediumSize.UseVisualStyleBackColor = true;
+            this.rdoMediumSize.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // rdoSmallSize
+            // 
+            this.rdoSmallSize.AutoSize = true;
+            this.rdoSmallSize.Location = new System.Drawing.Point(29, 37);
+            this.rdoSmallSize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.rdoSmallSize.Name = "rdoSmallSize";
+            this.rdoSmallSize.Size = new System.Drawing.Size(82, 19);
+            this.rdoSmallSize.TabIndex = 0;
+            this.rdoSmallSize.Text = "small ($10)";
+            this.rdoSmallSize.UseVisualStyleBackColor = true;
+            this.rdoSmallSize.CheckedChanged += new System.EventHandler(this.PizzaSize_CheckedChanged);
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(112, 267);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(116, 23);
+            this.txtTotal.TabIndex = 1;
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 270);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 363);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "M&L Pizza Palace";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoLargeSize;
+        private System.Windows.Forms.RadioButton rdoMediumSize;
+        private System.Windows.Forms.RadioButton rdoSmallSize;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Problem10/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem10
+{
+    public partial class Form1 : Form
+    {
+        float total;
+        public Form1()
+        {
+            InitializeComponent();
+            total = 0f;
+        }
+
+        private void PizzaSize_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdoSmallSize.Checked)
+            {
+                total = 10;
+            }
+            else if (rdoMediumSize.Checked)
+            {
+                total = 15;
+            }
+            else
+            {
+                total = 20;
+            }
+
+            txtTotal.Text = String.Format("{0:C}", total);
+
+        }
+    }
+}
